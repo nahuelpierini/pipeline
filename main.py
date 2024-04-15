26f8b35c4a525dce935cfd140186364a42fa617b
Nahuel Pierini <nahuelpierini@gmail.com>
2024-04-15 21:06:19 +0200

    word2 changed

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ def make_lower_case(word2: str):
 
 if __name__ == "__main__":
     word = make_upper_case("hola")
-    word2 = make_lower_case("HOLA")
+    word2 = make_lower_case("CHAU")
     print(word)
     print(word2)
